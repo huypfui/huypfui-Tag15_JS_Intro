2552c417abd9fe2dc38aa47394714ebf004b1750
huypfui <xuanduydanh@gmail.com>
2023-01-03 16:55:05 +0100

    update

---
+++ Lev1_12_js-einführung_arithmetic_operators/assets/js/main.js
@@ -0,0 +1,29 @@
+/* Multiplikation geht vor und so wird 5 * 10 zuerst durchgeführt.
+let score = 5 + 5 * 10;
+console.log('Ergebnis: ' + score); */
+
+/* // Du kannst Klammern() hinzufügen, um die Reihenfolge der Prioritäten zu ändern.
+let score = (5 + 5) * 10;
+console.log('Ergebnis: ' + score); */
+
+/* // Du kannst Werte auf Variablen addieren.
+let score = 0;
+score = score + 10;
+console.log('Ergebnis: ' + score); */
+
+/* // Hier von gibt es eine verkürzte Version.
+score += 10;
+console.log('Ergebnis: ' + score); */
+
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+
+
+zahl++;
+console.log('increment: ' + zahl);
+
+
+/* zahl--;
+console.log('dekrement: ' + zahl); */
