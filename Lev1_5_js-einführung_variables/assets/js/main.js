1f5377223ff9c3b212df6b233762eb002a56a731
huypfui <xuanduydanh@gmail.com>
2023-01-03 15:17:56 +0100

    update

---
+++ Lev1_5_js-einführung_variables/assets/js/main.js
@@ -0,0 +1,14 @@
+
+let carName = "BMW"
+console.log(carName)
+
+let x = 150
+let y = 50
+let z = x+y
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+let firstName = "John", lastName = "Doe", age = 35;
+console.log(firstName, lastName, age);
