bbf33073842d9b12c9842e17c7ec7a7380ae770d
huypfui <xuanduydanh@gmail.com>
2023-01-03 14:35:49 +0100

    first commit

---
+++ Lev1_1_js-einführung_console-log/assets/js/main.js
@@ -0,0 +1,25 @@
+
+a = 12;
+b = a * 3;
+console.log(b);
+
+document.write(b)
+
+console.log(document.URL)
+
+/* let anton = "Anton"
+console.log(anton) */
+
+let firstName ="Anton"
+console.log(firstName)
+
+let age = 28
+console.log(age)
+
+let job = "Trainer"
+console.log(job);
+
+let married = true 
+console.log(married);
+
+console.log(firstName + " ist " + age + " alt, von Beruf " + job + "und ist verheiratet: " + married);
---
+++ Lev1_7_js-einführung_coding-conventions/assets/js/main.js
@@ -0,0 +1,21 @@
+
+let helloWorld = "Hello World"
+console.log(helloWorld)
+
+const pi = 3.14
+console.log(pi);
+
+const burjDubai = "828m"
+console.log(burjDubai)
+
+let fullName = "Jan Schmidt"
+console.log(fullName)
+
+const eifelTower = "324m"
+console.log(eifelTower)
+
+const camelCase = "camelcase"
+console.log(camelCase)
+
+const kursStatus = "true"
+console.log(kursStatus);
