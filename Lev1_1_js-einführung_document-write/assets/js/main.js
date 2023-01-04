362faa3d802023978705d562feb9a3a4a2d12281
huypfui <xuanduydanh@gmail.com>
2023-01-04 17:43:08 +0100

    update

---
+++ Lev1_1_js-einführung_document-write/assets/js/main.js
@@ -0,0 +1,10 @@
+
+document.write("<h1>Hello World!</h1>");
+document.write("<p>Have a nice day!</p>")
+
+let myText = "Hello again";
+document.write(myText);
+
+let vorname = "<p>Name: Huy</p>";
+let nachname = "<p>Nachname: Danh</p>";
+document.write(vorname + nachname);
